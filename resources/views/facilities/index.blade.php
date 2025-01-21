a4ff2da4f5177aa988133d7e5a47d3ac3f7f300b
martgant <martgant@gmail.com>
2025-01-21 08:45:17 +0000

    Adding view for index

---
+++ resources/views/facilities/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facilities</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <h1>Facilities</h1>
+    <ul>
+        @foreach($facilities as $facility)
+            <li>
+                <strong>Name:</strong> {{ $facility->name }}<br>
+                <strong>Address:</strong> {{ $facility->address }}<br>
+                <strong>Phone:</strong> {{ $facility->phone }}<br>
+                <strong>Email:</strong> {{ $facility->email }}<br>
+                <strong>Description:</strong> {{ $facility->description }}
+            </li>
+        @endforeach
+    </ul>
+</body>
+</html>
