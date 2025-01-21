c1774792cb3786dca6b8d864d8b156a769abba71
martgant <martgant@gmail.com>
2025-01-21 08:41:53 +0000

    Adding FacilityController

---
+++ app/Http/Controllers/FacilityController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Facility;
+
+class FacilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $facilities = Facility::all();
+        return view('facilities.index', compact('facilities'));
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('facilities.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'description' => 'nullable',
+        ]);
+        
+        Facility::create($request->all());
+        
+        return redirect()->route('facilities.index')
+        ->with('success', 'Facility created successfully.');
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Facility  $facility
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Facility $facility)
+    {
+        return view('facilities.show', compact('facility'));
+    }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Facility  $facility
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Facility $facility)
+    {
+        return view('facilities.edit', compact('facility'));
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Facility  $facility
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Facility $facility)
+    {
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'description' => 'nullable',
+        ]);
+        
+        $facility->update($request->all());
+        
+        return redirect()->route('facilities.index')
+        ->with('success', 'Facility updated successfully.');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Facility  $facility
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Facility $facility)
+    {
+        $facility->delete();
+        
+        return redirect()->route('facilities.index')
+        ->with('success', 'Facility deleted successfully.');
+    }
+}
+
+
