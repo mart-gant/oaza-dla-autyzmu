a50d75d2b8e2f62991b5ea9fec8528dd3e397024
martgant <martgant@gmail.com>
2025-03-31 08:35:33 +0000

    Updating User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,57 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable;
+    
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'profile_photo',  // Zdjęcie profilowe
+        'interests',      // Zainteresowania
+        'support_preferences', // Preferencje wsparcia
+        'is_specialist',  // Czy użytkownik to specjalista?
+        'specialization', // Opis specjalizacji (dla specjalistów)
     ];
-
+    
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
+    
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'password' => 'hashed',
+        'is_specialist' => 'boolean',
+    ];
+    
+    /**
+     * Relacja użytkownika z placówkami.
+     */
+    public function facilities(): HasMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Facility::class);
     }
 }
