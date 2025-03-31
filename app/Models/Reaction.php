b9f9062af58e90572262a0c8c49952f065314777
martgant <martgant@gmail.com>
2025-03-31 09:00:35 +0000

    Add model Reaction

---
+++ app/Models/Reaction.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reaction extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['user_id', 'type', 'post_id', 'review_id'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function post()
+    {
+        return $this->belongsTo(ForumPost::class);
+    }
+    
+    public function review()
+    {
+        return $this->belongsTo(Review::class);
+    }
+}
